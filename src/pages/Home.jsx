a5e096956025fb604a3301007c08fa8e3d1c2156
mkkpkidangayam <mkkpkdm@gmail.com>
2024-04-29 11:25:57 +0530

    Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -135,8 +135,10 @@ const Home = () => {
 
       {renderCarousel()}
 
-      <div className="cursor-default">
-        <h1 className="text-center text-[300px] text-transparent bg-clip-text font-extrabold bg-cover bg-[url('https://m.media-amazon.com/images/G/01/Zappos/2024/Homepage/4.22/CROCS-STARWARS-AOE-1920x1000._FMwebp_.jpg')]">ZAPPOS</h1>
+      <div className="cursor-default border mx-5">
+        <h1 className="text-center text-[300px] text-transparent bg-clip-text font-extrabold bg-cover bg-[url('https://m.media-amazon.com/images/G/01/Zappos/2024/Homepage/4.22/CROCS-STARWARS-AOE-1920x1000._FMwebp_.jpg')]">
+          ZAPPOS
+        </h1>
       </div>
     </div>
   );
