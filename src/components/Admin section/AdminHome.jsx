749720a7fbd9e7f3aae63459075aebef19b404cc
mkkpkidangayam <mkkpkdm@gmail.com>
2024-06-13 12:05:40 +0530

    Updated

--- src/components/Admin section/AdminHome.jsx
+++ src/components/Admin section/AdminHome.jsx
@@ -4,13 +4,13 @@ import SecondChart from "./SecondChart";
 const AdminHome = () => {
   return (
     <div>
-      <div>
+      <div className="flex flex-col justify-center">
         <h1 className="text-center text-4xl font-bold font-serif text-green-800 py-2">
           Welcome Admin...
         </h1>
         <hr />
         <div className="flex flex-col justify-evenly h-screen">
-          <div className="flex justify-center">
+          <div className="flex justify-center absolute">
             <p className="text-center lg:text-[250px] text-[180px] text-transparent font-serif bg-clip-text font-extrabold bg-center bg-[url('https://media.licdn.com/dms/image/C561BAQEIPV76nJFFsQ/company-background_10000/0/1584409661241/zapposcom_cover?e=2147483647&v=beta&t=rmXnB4efRTXuqmIrTBVpNbmtZIqWG-uiTtkfuozpXvw')]">
               ZAPPOS
             </p>
