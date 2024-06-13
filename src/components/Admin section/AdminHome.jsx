43bf2b8b2440e72db91e9c9201ab9f721912df26
mkkpkidangayam <mkkpkdm@gmail.com>
2024-06-13 12:10:32 +0530

    Updated

--- src/components/Admin section/AdminHome.jsx
+++ src/components/Admin section/AdminHome.jsx
@@ -10,7 +10,7 @@ const AdminHome = () => {
         </h1>
         <hr />
         <div className="flex flex-col justify-evenly h-screen">
-          <div className="flex justify-center absolute">
+          <div className="flex justify-center">
             <p className="text-center lg:text-[250px] text-[180px] text-transparent font-serif bg-clip-text font-extrabold bg-center bg-[url('https://media.licdn.com/dms/image/C561BAQEIPV76nJFFsQ/company-background_10000/0/1584409661241/zapposcom_cover?e=2147483647&v=beta&t=rmXnB4efRTXuqmIrTBVpNbmtZIqWG-uiTtkfuozpXvw')]">
               ZAPPOS
             </p>
