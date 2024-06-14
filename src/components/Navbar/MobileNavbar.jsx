6ee0625ff893d0911cc0e0dbc0ce711ff1b0f548
mkkpkidangayam <mkkpkdm@gmail.com>
2024-06-14 09:57:18 +0530

    Updated

--- src/components/Navbar/MobileNavbar.jsx
+++ src/components/Navbar/MobileNavbar.jsx
@@ -133,7 +133,7 @@ const MobileNavbar = () => {
         </div>
       </div>
       {isNavOpen && (
-        <div className="p-4 border-b absolute">
+        <div className="p-4 border-b absolute bg-slate-200">
           <form
             onSubmit={handleSearchSubmit}
             className="flex items-center mb-4"
