3ec8657293d6ea59da3c5a3c0033861139fd4d7e
mkkpkidangayam <mkkpkdm@gmail.com>
2024-06-15 09:18:08 +0530

    Updated

--- src/pages/ProductsListing.jsx
+++ src/pages/ProductsListing.jsx
@@ -77,7 +77,7 @@ const ProductsListing = () => {
   return (
     <div className="container my-6">
       <div className="flex flex-col md:flex-row justify-between items-center mb-6">
-        <div className="md:w-60 ml-10 mb-4 md:mb-0">
+        <div className="md:w-60 md:ml-10 mb-4 md:mb-0">
           <h1 className="font-bold text-4xl capitalize">{"Collections"}</h1>
           <p className="text-gray-600">({productCount}) items found</p>
         </div>
