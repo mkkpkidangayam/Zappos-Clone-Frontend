24c70ec212c02703b507b98476ba441edeaf642c
mkkpkidangayam <mkkpkdm@gmail.com>
2024-07-22 17:41:47 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import MainPage from "./MainPage";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
-  
   return (
     <GoogleOAuthProvider clientId="134165473966-0dnln141c85h92cuqi64dnbiebuk8bv6.apps.googleusercontent.com">
       <BrowserRouter>
